fb8b94c617ebd0d06d102c6ae9145b0000a263fb
Vegetog <110553275+Vegetog@users.noreply.github.com>
2024-11-17 16:11:48 +0800

    Update site.config.ts

--- site.config.ts
+++ site.config.ts
@@ -2,7 +2,7 @@ import { siteConfig } from './lib/site-config'
 
 export default siteConfig({
   // the site's root Notion page (required)
-  rootNotionPageId: '141b26d6e27b805a939dc5410d37314b',
+  rootNotionPageId: 'Notion-Blog-141b26d6e27b803fb75dfb458d4940f0',
 
   // if you want to restrict pages to a single notion workspace (optional)
   // (this should be a Notion ID; see the docs for how to extract this)
